08a2623d5b5d5eef1998429ac03407f9b962e02d
Drew <saladbar821@gmail.com>
2018-03-22 15:34:23 -0400

    Update munki_enroll.sh

--- Scripts/munki_enroll.sh
+++ Scripts/munki_enroll.sh
@@ -1,52 +1,81 @@
-#!/bin/sh 
+#!/bin/sh
+
+# Change this URL to the location of your Munki Enroll install
+SUBMITURL="http://localhost:8888/munki/munki-enroll/enroll.php"
+
+# Computer name format: TLA-(SUBTLA)-TYPE-NUM (ex: LAW-LIB-ML-0001)
+# Set previously as part of deployment.
 
 # Gather computer information
-IDENTIFIER=$( defaults read /Library/Preferences/ManagedInstalls ClientIdentifier ); 
-HOSTNAMETEMP=$( scutil --get ComputerName );
-# Subs in underscores for spaces in the HOSTNAME
-HOSTNAME=${HOSTNAMETEMP// /_}
+TEMPHOSTNAME=$( scutil --get ComputerName )
+# Convert to uppercase for consistency, in the rare case it isn't already.
+HOSTNAME=$( echo "$TEMPHOSTNAME" | awk '{ print toupper($0) }' )
 
-if [ ! -z "$IDENTIFIER" ]; then
+# Determine number of fields in the computer name for analysis. Should be
+# three or four based on our convention and whether a sub-tla exists.
+NAMEFIELDS=$( echo "$HOSTNAME" | awk -F "-" '{ print NF }' )
 
-	# Change this URL to the location of your Munki Enroll install
-	SUBMITURL="http://localhost:8888/munki/munki-enroll/enroll.php"
+if [ "$NAMEFIELDS" == "3" ]; then
 
-	# Test the connection to the server
-	SHORTURL=$(echo "$SUBMITURL" | awk -F/ '{print $3}')
-	PINGTEST=$(ping -o "$SHORTURL" | grep "64 bytes")
+	# Name does not include a sub-tla.
 
-	if [ ! -z "$PINGTEST" ]; then
+	TLA=$( echo "$HOSTNAME" | awk -F "-" '{ print $1 }' )
+	SUBTLA=
+	TYPE=$( echo "$HOSTNAME" | awk -F "-" '{ print $2 }' )
 
-	    # Application paths
-	    CURL="/usr/bin/curl"
+elif [ "$NAMEFIELDS" == "4" ]; then
 
-	    $CURL --max-time 5 --silent --get \
-	        -d hostname="$HOSTNAME" \
-	        -d identifier="$IDENTIFIER" \
-	        "$SUBMITURL"
+	# Name contains sub-tla
 
-	    # This is a fix for clients based on a manifest in the root /manifests directory
-	    # See GitHub issue No. 5
-	    if [ $( echo "$IDENTIFIER" | grep "/" ) ]
-	    then
-	    IDENTIFIER_PATH=$( echo "$IDENTIFIER" | sed 's/\/[^/]*$//' ); 
-	    defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "$IDENTIFIER_PATH/clients/$HOSTNAME"
-	    else
-	     defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "clients/$HOSTNAME"
-	    fi
- 
-	    exit 0
-
-	else
-	
-	    # No good connection to the server
-	    exit 1
-
-	fi
+	TLA=$( echo "$HOSTNAME" | awk -F "-" '{ print $1 }' )
+	SUBTLA=$( echo "$HOSTNAME" | awk -F "-" '{ print $2 }' )
+	TYPE=$( echo "$HOSTNAME" | awk -F "-" '{ print $3 }' )
 
 else
 
-	echo "A ClientIdentifier must be set before running this script!"
+	# Name does not follow standard convention.
 	exit 1
-	
+
+fi
+
+# Validate TYPE field and convert to a human identifier, if valid.
+# Some cases exist where this may not be valid, so we'll null the variable
+# in that case.
+
+CHASSIS=
+
+if [ "$TYPE" == "ML" ]; then
+
+	CHASSIS="laptop"
+
+elif [ "$CHASSIS" == "MD" ]; then
+
+	CHASSIS="desktop"
+
+fi
+
+
+# Test the connection to the server
+SHORTURL=$( echo "$SUBMITURL" | awk -F/ '{print $3}' )
+PINGTEST=$( ping -o "$SHORTURL" | grep "64 bytes" )
+
+if [ ! -z "$PINGTEST" ]; then
+
+  # Application paths
+  CURL="/usr/bin/curl"
+
+  $CURL --max-time 5 --silent --get \
+      -d hostname="$HOSTNAME" \
+      -d tla="$TLA" \
+			$(if [ -n "$SUBTLA" ]; then echo "-d $SUBTLA"; fi) \
+			$(if [ -n "$CHASSIS" ]; then echo "-d $CHASSIS"; fi) \
+      "$SUBMITURL"
+
+	exit 0
+
+else
+
+	# No good connection to the server
+	exit 2
+
 fi
